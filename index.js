1f74c6aee512e55cc94ed89780b6b937cc4038c2
janaemarie_@hotmail.com sweet <janaemarie_@hotmail.com>
2018-05-31 20:08:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 var name = "Susan"
 var height = "70"
-if (name === "Susan") {
-    console.log("The name is correct")
-}
-if (height === "70") {
-    console.log("The height is correct")
-}
+
 // Don't worry about this
 module.exports = { name, height
 }
